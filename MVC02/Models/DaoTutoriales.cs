a83406e98ef7485420cea1251850af1d16c8622d
Gustavo Chimoy Garcia <gustavo_fred@hotmail.com>
2022-06-11 18:35:09 -0500

    actualizaciones

---
+++ MVC02/Models/DaoTutoriales.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC02.Models
+{
+    public class DaoTutoriales
+    {
+        public List<tutoriales> tutorialesQry()
+        {
+            List<tutoriales> list;
+
+            using (var db = new ModelTutoriales())
+            {
+                var query = from t in db.tutoriales select t;
+                list = query.ToList<tutoriales>();
+            }
+            
+            return list;
+        }
+    }
+}
--- MVC02/js/main.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $("h4").mouseover(function() {
-        $(this).addClass("text-end");
-        
-    }).mouseout(function() {
-        $(this).removeClass("text-end");
-    });
-});
